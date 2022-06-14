5233f2bc82ba7cdeba99bd7f3e1b16b92abc70d7
Esper0328 <esper0328@gmail.com>
2022-06-15 08:47:33 +0900

    First Power Control Implementation.

---
+++ PowerControl.c
@@ -0,0 +1,7 @@
+#include "PowerControl.h"
+
+#include <stdio.h>
+
+void PowerControlStart() { printf("Power Control Start\n"); }
+
+void PowerControlStop() { printf("Power Control Stop\n"); }
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "PowerControl.h"
+
+int main() {
+    PowerControlStart();
+    PowerControlStop();
+    return 0;
+}
